9e38471a2aa356b1f787cd8b4bc678af43de7d84
maive <vero.larrondo.r@gmail.com>
2024-08-01 20:37:13 +0200

    change of file extension to convert to typescript

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import gsap from "gsap";
+import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { heroVideo, smallHeroVideo } from "../utils";
 
